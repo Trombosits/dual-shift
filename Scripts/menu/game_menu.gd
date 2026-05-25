cd3a1558fca5ad915e25bff970a026183d95e23f
Aditia Agustin Sudrajat <aditiponk@gmail.com>
2026-05-25 13:54:35 +0700

    fix_ui

---
+++ Scripts/menu/game_menu.gd
@@ -0,0 +1,55 @@
+extends Control
+
+var button_type = null
+
+
+func _ready():
+	$Fade_Transition.show()
+	$Fade_Transition/Timer.start()
+	$Fade_Transition/AnimationPlayer.play("Fade_Out")
+
+func _on_game_1_pressed():
+	button_type = "game1"
+	$Fade_Transition.show()
+	$Fade_Transition/Timer.start()
+	$Fade_Transition/AnimationPlayer.play("Fade_In")
+
+func _on_game_2_pressed():
+	button_type = "game2"
+	$Fade_Transition.show()
+	$Fade_Transition/Timer.start()
+	$Fade_Transition/AnimationPlayer.play("Fade_In")
+
+func _on_game_3_pressed():
+	button_type = "game3"
+	$Fade_Transition.show()
+	$Fade_Transition/Timer.start()
+	$Fade_Transition/AnimationPlayer.play("Fade_In")
+
+func _on_skill_tree_pressed():
+	button_type = "skillTree"
+	$Fade_Transition.show()
+	$Fade_Transition/Timer.start()
+	$Fade_Transition/AnimationPlayer.play("Fade_In")
+
+func _on_main_menu_pressed():
+	button_type = "mainMenu"
+	$Fade_Transition.show()
+	$Fade_Transition/Timer.start()
+	$Fade_Transition/AnimationPlayer.play("Fade_In")
+
+func _on_timer_timeout() -> void:
+	if button_type == "game1":
+		get_tree().change_scene_to_file("res://Scenes/gamemodes/fifo/FIFOGame.tscn")
+
+	elif button_type == "game2":
+		get_tree().change_scene_to_file("res://Scenes/gamemodes/filo_sorting/FILOSortingGame.tscn")
+
+	elif button_type == "game3":
+		get_tree().change_scene_to_file("res://Scenes/Typing.tscn")
+
+	elif button_type == "skillTree":
+		get_tree().change_scene_to_file("res://Scenes/SkillTree.tscn")
+
+	elif button_type == "mainMenu":
+		get_tree().change_scene_to_file("res://Scenes/menu/main_menu.tscn")
---
+++ Scripts/menu/main_menu.gd
@@ -0,0 +1,46 @@
+extends Control
+
+var button_type = null
+@onready var main_button: VBoxContainer = $Main_Button
+@onready var setting: Panel = $Setting
+
+func _ready():
+	$Fade_Transition.show()
+	$Fade_Transition/Timer.start()
+	$Fade_Transition/AnimationPlayer.play("Fade_Out")
+	main_button.visible = true
+	setting.visible = false
+
+func _on_new_game_pressed():
+	button_type = "newGame"
+	$Fade_Transition.show()
+	$Fade_Transition/Timer.start()
+	$Fade_Transition/AnimationPlayer.play("Fade_In")
+
+func _on_continue_pressed():
+	button_type = "continue"
+	$Fade_Transition.show()
+	$Fade_Transition/Timer.start()
+	$Fade_Transition/AnimationPlayer.play("Fade_In")
+
+func _on_setting_pressed():
+	main_button.visible = false
+	setting.visible = true
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+func _on_timer_timeout():
+	if button_type == "newGame":
+		get_tree().change_scene_to_file("res://Scenes/gamemodes/filo_sorting/FILOSortingGame.tscn")
+	
+	elif button_type == "continue":
+		get_tree().change_scene_to_file("res://Scenes/menu/game_menu.tscn")
+	
+	elif button_type == "setting":
+		print("Masuk menu setting")
+
+
+func _on_back_pressed():
+	main_button.visible = true
+	setting.visible = false
